1985d82d2b875de1d1886116ccd89f9d7cfa5f32
Pavankasala <pavankasala999@gmail.com>
2025-09-02 23:18:18 +0530

    commited

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -2,8 +2,10 @@
 import axios from "axios";
 
 export const API_BASE_URL = "https://farmtrak.onrender.com/api";
+console.log("API_BASE_URL:", API_BASE_URL);
 
 export const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: API_BASE_URL, // use the same URL here
   withCredentials: true,
 });
+
